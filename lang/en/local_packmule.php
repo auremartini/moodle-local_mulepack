6bbf02024ad4c1ab46177eafd226f4a760249cc9
Aureliano Martini <aure.martini@gmail.com>
2023-09-16 15:31:04 +0200

    Init version.php

---
+++ lang/en/local_packmule.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local plugin "PackMule - sync Moodle to GAGI database exports" - Version file
+ *
+ * @package    local_packmule
+ * @copyright  2023, Aureliano Martini (Liceo cantonale di Lugano 3) <aureliano.martini@edu.ti.ch>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+//Plugin text
+$string['pluginname'] = 'PackMule';
+$string['plugin_navbar_lavel'] = 'PackMule: sync from GAGI';
+$string['plugin_page_title'] = 'PackMule: sync from GAGI';
